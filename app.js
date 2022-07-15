11f13a8b176b05b43f0ff59854160438b6e5c2a1
Tomoro <tomoro0726@gmail.com>
2022-07-15 21:08:39 +0900

    Post検知完了

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.use(express.static("public"));
 
 const mysql = require("mysql2");
 
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
 const connection = mysql.createConnection({
   host: "192.168.100.4",
   user: "summer",
@@ -20,6 +26,29 @@ app.get("/",(req,res)=>{
     'SELECT*FROM media',
     (errow, result) => {
       console.log(result);
+    }
+  );
+});
+
+app.get("/set",(req,res)=>{
+  res.render("set.ejs");
+  connection.query(
+    'SELECT*FROM media',
+    (errow, result) => {
+      console.log(result);
+    }
+  );
+});
+
+app.post("/vimeo",(req,res)=>{
+  console.log(req.body);
+  connection.query(
+    'SELECT*FROM INSERT INTO media (name) VALUES (?);',
+    [req.body.name],
+    (errow, result) => {
+      console.log(errow);
+      console.log(req.body);
+      res.redirect("/set");
     }
   );
 });
