b1477ed762a9828d10c91d6486296ece7d7a969e
Tomoro <tomoro0726@gmail.com>
2022-07-15 18:41:40 +0900

    接続テスト

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require("express");
 const app=express();
 app.use(express.static("public"));
 
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "192.168.100.4",
+  user: "admintest",
+  password: "Pass@word1",
+  port : 3306,
+  database: "free_db"
+});
+
+
+connection.connect();
+
+connection.query('SELECT * from test_table LIMIT 10;', (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows);
+});
+
+connection.end();
 
 app.get("/",(req,res)=>{
   res.render("index.ejs")
