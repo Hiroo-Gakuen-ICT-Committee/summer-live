03eff3644ebb436aee0a27674f60d32289885b8a
Tomoro <tomoro0726@gmail.com>
2022-07-15 20:44:28 +0900

    形にはなった

--- public/index.js
+++ public/index.js
@@ -1,11 +1,23 @@
-var titile1=anime.timeline({
-  duration: 5000
+anime({
+  targets: '.title1',
+  opacity: [
+    {
+      value: '1',
+      duration: 300,
+      delay: 600,
+      easing: 'linear'
+    },
+    {
+      value: '0',
+      duration: 1000,
+      delay: 600,
+      easing: 'linear'
+    },
+  ],
 });
-
-titile1.add({
-  targets: '.title1',
-  translateX: 250,
-}).add({
-  targets: '.title1',
-  translateX: -250,
-})
+anime({
+  targets: '.vimeo',
+  duration: 1000,
+  delay: 2500,
+  opacity:1
+});
